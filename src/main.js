a032185976b24cfb67ae65859c1804a81e5c5052
9 Melody <minhdaik9x@gmail.com>
2022-02-18 09:51:10 +0700

    - init plugins

--- src/main.js
+++ src/main.js
@@ -3,11 +3,16 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "@/assets/styles/main.css";
+import vuetify from "./plugins/vuetify";
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
+  i18n,
   render: (h) => h(App),
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    i18n: {
+      locale: "vi",
+      fallbackLocale: "vi",
+      localeDir: "locales",
+      enableInSFC: false,
+      includeLocales: false,
+      enableBridge: true,
+    },
+  },
 });
